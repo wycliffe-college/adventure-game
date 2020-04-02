b25b27295b1eefef5aeedcca360590a37b2beeac
Josia <chanj@wycliffe.co.uk>
2020-04-02 19:57:11 +0800

    vertical line + physics changes

--- src/Josias-orginal-level.js
+++ src/Josias-orginal-level.js
@@ -1,7 +1,7 @@
 //Josia's Level, based upon Brendan's Level, based upon level1.js
 
-const friction = 10.0;
-const density = 10.0;
+const friction = 15.0;
+const density = 5.0;
 
 export function createLevel(world) {
     var groundFD = {density: density, friction: friction};
@@ -9,5 +9,5 @@ export function createLevel(world) {
     world.createBody(Vec2(0,0)).createFixture(planck.Box(20.0, 0.25), groundFD);
 
     world.createBody().createFixture(planck.Edge(Vec2(0.0, -100.0), Vec2(0.0, 100.0)), groundFD);
-    world.createBody().createFixture(planck.Edge(Vec2(-100.0, 0.0), Vec2(100.0, 0.0)), groundFD);
+    world.createBody().createFixture(planck.Edge(Vec2(-10.0, 0.0), Vec2(10.0, 0.0)), groundFD);
 }
