9177868ab59c2b7f718fb254dbf0ab3e867bca43
Josia <chanj@wycliffe.co.uk>
2020-04-02 19:43:30 +0800

    the thing forced me to write a comment

--- src/Josias-orginal-level.js
+++ src/Josias-orginal-level.js
@@ -1,10 +1,11 @@
 //Josia's Level, based upon Brendan's Level, based upon level1.js
 
-const friction = 0.9;
-const density = 0.5;
+const friction = 10.0;
+const density = 10.0;
 
 export function createLevel(world) {
     var groundFD = {density: density, friction: friction};
 
-    world.createBody().createFixture(planck.Edge(Vec2(-100.0, 0.0), Vec2(100.0, 0.0)), groundFD)
+    world.createBody().createFixture(planck.Edge(Vec2(0.0, -100.0), Vec2(0.0, 100.0)), groundFD);
+    world.createBody().createFixture(planck.Edge(Vec2(0.0, -100.0), Vec2(0.0, 100.0)), groundFD)
 }
