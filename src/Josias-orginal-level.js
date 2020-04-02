1556e5cc79f0e81e3c1cbe5acb99c5a624208642
Josia <chanj@wycliffe.co.uk>
2020-04-02 19:40:54 +0800

    tempered with physics and resolved my level to 1 line

--- src/Josias-orginal-level.js
+++ src/Josias-orginal-level.js
@@ -1,14 +1,10 @@
 //Josia's Level, based upon Brendan's Level, based upon level1.js
 
-const friction = 0.6;
-const density = 0.0;
+const friction = 0.9;
+const density = 0.5;
 
 export function createLevel(world) {
     var groundFD = {density: density, friction: friction};
 
-    world.createBody().createFixture(planck.Edge(Vec2(-100.0, 0.0), Vec2(100.0, 0.0)), groundFD);
-    world.createBody().createFixture(planck.Edge(Vec2(-10.0, 1.0), Vec2(-10.0, 5.0)), groundFD);
-    world.createBody().createFixture(planck.Edge(Vec2(-20.0, 1.0), Vec2(-20.0, 5.0)), groundFD);
-    world.createBody().createFixture(planck.Edge(Vec2(-30.0, 1.0), Vec2(-30.0, 5.0)), groundFD);
-    world.createBody().createFixture(planck.Edge(Vec2(-40.0, 1.0), Vec2(-40.0, 5.0)), groundFD);
+    world.createBody().createFixture(planck.Edge(Vec2(-100.0, 0.0), Vec2(100.0, 0.0)), groundFD)
 }
