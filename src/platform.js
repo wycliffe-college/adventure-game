3f7960617d1a65138d3fafee15d8b9f338131b04
brend <indb@wycliffe.co.uk>
2020-04-09 13:51:02 +0100

    Simplified the platform script

--- src/platform.js
+++ src/platform.js
@@ -1,13 +1,20 @@
-export function createPlatform(world, position, width) {
+export function createPlatform(world, position, width, imageAddress="./images/platform_brickwall.png") {
+
+    //Make the platform as an object
     const platform = world.createBody(position);
+
+    //Fix the platform to the world with a width and fixed height of 0.25
     platform.createFixture(planck.Box(width, 0.25), 1.0);
 
+    //Load image from address
     const img = new Image();
-    img.src = "images/explorer.png";
+    img.src = imageAddress;
+
+    //When the image loads, run the function...
     img.onload = () => {
-        platform.render = {
+        img.render = {
             custom: (ctx, pos, size) => {
-                ctx.drawImage(img, pos.x, pos.y, size.width, size.height);
+                ctx.drawImage(img, pos.x, pos.y, (size.width), (size.height));
                 return true // don't draw bounding box
             }
         }
