d3f5a2dd41579940c06e993df5ba1d676e3fbd6a
brend <indb@wycliffe.co.uk>
2020-04-09 14:47:03 +0100

    created box.js to make dynamic boxes, similar to platform.js

---
+++ src/box.js
@@ -0,0 +1,25 @@
+//Created a file for maing dynamic objects, boxes.
+
+export function createBox(world, position, width, height, density=1, imageAddress="./images/Crest_White.png") {
+
+    //Make the platform as an object
+    const box = world.createBody(position);
+
+    //Fix the platform to the world with a width and fixed height of 0.25
+    box.createFixture(planck.Box(width, height),density);
+
+    //Load image from address
+    const img = new Image();
+    img.src = imageAddress;
+
+    //When the image loads, run the function...
+    img.onload = () => {
+        platform.render = {
+            custom: (ctx, pos, size) => {
+                ctx.drawImage(img, pos.x, pos.y, size.width, size.height);
+                return true // don't draw bounding box
+            }
+        }
+    };
+    return platform;
+}
