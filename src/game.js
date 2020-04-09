0a9fe9d0d4f758424ae10e1c6238049e234d87af
brend <indb@wycliffe.co.uk>
2020-04-09 13:53:13 +0100

    hardwired the parse function to go to brendans-level.js by default

--- src/game.js
+++ src/game.js
@@ -14,7 +14,7 @@ var levels = { "default" : defaultLevel , "josia":josiaLevel , "brendan" :brenda
 function parseHtmlParameters()
 {
     //Add parameters and their default values to this dictionary
-    var params = {"level":"default"};
+    var params = {"level":"brendan"}; //BRENDAN CHANGED THIS TO ACCESS HIS LEVEL
 
     var parameters = location.search.substring(1).split("&");
     var index;
