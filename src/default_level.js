0e9cd4a45721b6de4520402b1b38be0a602c29da
wycliffe-mw <mark.waller@wycliffe.co.uk>
2020-05-07 13:45:18 +0100

    start of lava

--- src/default_level.js
+++ src/default_level.js
@@ -1,11 +1,16 @@
 import { Vec2, Box } from "./planck-module.js";
-import {createPlatform} from "./grassplatform.js";
+import { createPlatform } from './platform.js';
+import {grass} from "./grass_definition.js";
+import {lava} from "./lava_definition.js";
 import {createDoor} from "./leveldoor.js"
 
 export function createLevel(world) {
-    createPlatform(world, Vec2(-10,-4), 5);
-    createPlatform(world, Vec2(0,0), 10);
-    createPlatform(world, Vec2(15,-8), 20);
+    createPlatform(world, -10,-4, 5,grass);
+    createPlatform(world, 0,0, 10,grass);
+    createPlatform(world, 15,-8, 20,grass);
+
+    createPlatform(world, 0, 10, 40, lava);
+
     var door = createDoor(world , Vec2(25,-12) );
     return door;
 }
