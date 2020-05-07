10581ac596c11c50b5e4161731a7417182af2a23
brend <indb@wycliffe.co.uk>
2020-05-07 14:57:10 +0100

    platform changes

--- src/levels/brendans-level.js
+++ src/levels/brendans-level.js
@@ -22,10 +22,10 @@ export function createLevel(world) {
     createPlatform(world, -19, 5, 10, ruined_forest); //Left platform where box lands
     createPlatform(world, -10, 8, 1, ruined_forest); //single left platform to jump on before box platform
     createPlatform(world, 6, 2, 5, ruined_forest); //Upper medium sized platform over main spawn
-    createPlatform(world, -30, 5, 1, ruined_forest); //single platform to hold box from falling off
-    createPlatform(world, -12, -5, 10, ruined_forest); //platform with door on it
-    createBox(world, Vec2(-28,2),2,2, 10);
-    createPlatform(world, -19, -3, 1, ruined_forest); //single platform attached to door plat left
+    createPlatform(world, -26, 2, 2, ruined_forest); //single platform to hold box from falling off
+    createPlatform(world, -9, -4, 10, ruined_forest); //platform with door on it
+    createBox(world, Vec2(-16,2),2,2, 10);
+    createPlatform(world, -15.5, -3, 1, ruined_forest); //single platform attached to door plat left
     createPlatform(world, -3, -2, 1, ruined_forest); //single platform attached to door plat right
 
 
