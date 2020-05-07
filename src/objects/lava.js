16bdfccea260dc578afd58bdf9738096b8f4a526
wycliffe-mw <mark.waller@wycliffe.co.uk>
2020-05-07 14:00:20 +0100

    start of lava

---
+++ src/objects/lava.js
@@ -0,0 +1,6 @@
+import {lava} from "../definitions/lava_definition.js";
+import {createPlatform} from "./platform.js";
+
+export function createLava( xPos , yPos , width ) {
+    returen createPlatform(world, 0, 10, 40, lava);
+}
